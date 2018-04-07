1c0fac604a13c43c0b2be60e0af8b91defb66ce2
primaeval <primaeval@users.noreply.github.com>
2018-04-07 17:04:46 +0200

    koditvepg.com

--- main.py
+++ main.py
@@ -86,10 +86,18 @@ def update():
 
         filename = xbmc.translatePath("special://profile/addon_data/plugin.video.xmltv.meld/" + url.rsplit('/',1)[-1])
         xbmcvfs.copy(url,filename)
-        f = open(filename+".xml","w")
-        subprocess.call(["busybox64","xz","-dc",filename],stdout=f,shell=True)
-        f.close()
-        data = xbmcvfs.File(filename+'.xml','r').read()
+        if filename.endswith('.xz'):
+            f = open(filename+".xml","w")
+            subprocess.call(["busybox","xz","-dc",filename],stdout=f,shell=True)
+            f.close()
+            data = xbmcvfs.File(filename+'.xml','r').read()
+        elif filename.endswith('.gz'):
+            f = open(filename[:-3],"w")
+            subprocess.call(["busybox","gunzip","-dc",filename],stdout=f,shell=True)
+            f.close()
+            data = xbmcvfs.File(filename[:-3],'r').read()
+        else:
+            data = xbmcvfs.File(filename,'r').read()
 
         channels = re.findall('(<channel.*?</channel>)', data, flags=(re.I|re.DOTALL))
         programmes = re.findall('(<programme.*?</programme>)', data, flags=(re.I|re.DOTALL))
@@ -159,10 +167,19 @@ def get_xmltv():
 def select_channels(url):
     filename = xbmc.translatePath("special://profile/addon_data/plugin.video.xmltv.meld/" + url.rsplit('/',1)[-1])
     xbmcvfs.copy(url,filename)
-    f = open(filename+".xml","w")
-    subprocess.call(["busybox","xz","-dc",filename],stdout=f,shell=True)
-    f.close()
-    data = xbmcvfs.File(filename+'.xml','r').read()
+
+    if filename.endswith('.xz'):
+        f = open(filename+".xml","w")
+        subprocess.call(["busybox","xz","-dc",filename],stdout=f,shell=True)
+        f.close()
+        data = xbmcvfs.File(filename+'.xml','r').read()
+    elif filename.endswith('.gz'):
+        f = open(filename[:-3],"w")
+        subprocess.call(["busybox","gunzip","-dc",filename],stdout=f,shell=True)
+        f.close()
+        data = xbmcvfs.File(filename[:-3],'r').read()
+    else:
+        data = xbmcvfs.File(filename,'r').read()
 
     htmlparser = HTMLParser()
 
@@ -226,6 +243,34 @@ def rytec_xmltv():
     return items
 
 
+@plugin.route('/koditvepg_xmltv')
+def koditvepg_xmltv():
+
+    urls = {'http://epg.koditvepg.com/AT/guide.xml.gz': 'Austria', 'http://epg.koditvepg.com/PL/guide.xml.gz': 'Poland', 'http://epg.koditvepg.com/TR/guide.xml.gz': 'Turkey', 'http://epg.koditvepg.com/IN/guide.xml.gz': 'India', 'http://epg.koditvepg.com/FI/guide.xml.gz': 'Finland', 'http://epg.koditvepg.com/SK/guide.xml.gz': 'Slovakia', 'http://epg.koditvepg.com/CN/guide.xml.gz': 'China', 'http://epg.koditvepg.com/NL/guide.xml.gz': 'Netherlands', 'http://epg.koditvepg.com/GE/guide.xml.gz': 'Georgia', 'http://epg.koditvepg.com/LU/guide.xml.gz': 'Luxembourg', 'http://epg.koditvepg.com/SE/guide.xml.gz': 'Sweden', 'http://epg.koditvepg.com/RU/guide.xml.gz': 'Russia', 'http://epg.koditvepg.com/AU/guide.xml.gz': 'Australia', 'http://epg.koditvepg.com/IS/guide.xml.gz': 'Iceland', 'http://epg.koditvepg.com/AR/guide.xml.gz': 'Argentina', 'http://epg.koditvepg.com/GB/guide.xml.gz': 'United Kingdom', 'http://epg.koditvepg.com/RO/guide.xml.gz': 'Romania', 'http://epg.koditvepg.com/ME/guide.xml.gz': 'Montenegro', 'http://epg.koditvepg.com/NZ/guide.xml.gz': 'New Zealand', 'http://epg.koditvepg.com/DE/guide.xml.gz': 'Germany', 'http://epg.koditvepg.com/DO/guide.xml.gz': 'Dominican Rep.', 'http://epg.koditvepg.com/BR/guide.xml.gz': 'Brazil', 'http://epg.koditvepg.com/TH/guide.xml.gz': 'Thailand', 'http://epg.koditvepg.com/DK/guide.xml.gz': 'Denmark', 'http://epg.koditvepg.com/PH/guide.xml.gz': 'Philippines', 'http://epg.koditvepg.com/AL/guide.xml.gz': 'Albania', 'http://epg.koditvepg.com/PR/guide.xml.gz': 'Puerto Rico', 'http://epg.koditvepg.com/RS/guide.xml.gz': 'Serbia', 'http://epg.koditvepg.com/GR/guide.xml.gz': 'Greece', 'http://epg.koditvepg.com/PA/guide.xml.gz': 'Panama', 'http://epg.koditvepg.com/IE/guide.xml.gz': 'Ireland', 'http://epg.koditvepg.com/TW/guide.xml.gz': 'Taiwan', 'http://epg.koditvepg.com/JP/guide.xml.gz': 'Japan', 'http://epg.koditvepg.com/MX/guide.xml.gz': 'Mexico', 'http://epg.koditvepg.com/FR/guide.xml.gz': 'France', 'http://epg.koditvepg.com/AE/guide.xml.gz': 'United Arab Emirates', 'http://epg.koditvepg.com/MK/guide.xml.gz': 'Macedonia', 'http://epg.koditvepg.com/HU/guide.xml.gz': 'Hungary', 'http://epg.koditvepg.com/IL/guide.xml.gz': 'Israel', 'http://epg.koditvepg.com/SA/guide.xml.gz': 'Saudi Arabia', 'http://epg.koditvepg.com/UA/guide.xml.gz': 'Ukraine', 'http://epg.koditvepg.com/PK/guide.xml.gz': 'Pakistan', 'http://epg.koditvepg.com/LT/guide.xml.gz': 'Lithuania', 'http://epg.koditvepg.com/KZ/guide.xml.gz': 'Kazakhstan', 'http://epg.koditvepg.com/LV/guide.xml.gz': 'Latvia', 'http://epg.koditvepg.com/BE/guide.xml.gz': 'Belgium', 'http://epg.koditvepg.com/PT/guide.xml.gz': 'Portugal', 'http://epg.koditvepg.com/CA/guide.xml.gz': 'Canada', 'http://epg.koditvepg.com/VN/guide.xml.gz': 'Vietnam', 'http://epg.koditvepg.com/HR/guide.xml.gz': 'Croatia', 'http://epg.koditvepg.com/ES/guide.xml.gz': 'Spain', 'http://epg.koditvepg.com/CZ/guide.xml.gz': 'Czech Rep.', 'http://epg.koditvepg.com/EG/guide.xml.gz': 'Egypt', 'http://epg.koditvepg.com/BG/guide.xml.gz': 'Bulgaria', 'http://epg.koditvepg.com/CO/guide.xml.gz': 'Colombia', 'http://epg.koditvepg.com/US/guide.xml.gz': 'United States', 'http://epg.koditvepg.com/NO/guide.xml.gz': 'Norway', 'http://epg.koditvepg.com/BA/guide.xml.gz': 'Bosnia and Herz.', 'http://epg.koditvepg.com/CH/guide.xml.gz': 'Switzerland', 'http://epg.koditvepg.com/IT/guide.xml.gz': 'Italy', 'http://epg.koditvepg.com/SI/guide.xml.gz': 'Slovenia', 'http://epg.koditvepg.com/XK/guide.xml.gz': 'Kosovo'}
+
+    items = []
+    xmltv = plugin.get_storage('xmltv')
+    for url,description in urls.iteritems():
+
+        context_items = []
+        if url not in xmltv:
+            context_items.append(("Add xmltv", 'XBMC.RunPlugin(%s)' % (plugin.url_for(add_xmltv, name=description, url=url))))
+            label = description
+        else:
+            context_items.append(("Remove xmltv", 'XBMC.RunPlugin(%s)' % (plugin.url_for(delete_xmltv, url=url))))
+            label = "[COLOR yellow]%s[/COLOR]" % description
+
+        items.append(
+        {
+            'label': label,
+            'path': plugin.url_for('select_channels',url=url),
+            'thumbnail':get_icon_path('settings'),
+            'context_menu': context_items,
+        })
+
+    return sorted(items, key = lambda x: remove_formatting(x["label"]))
+
+
 @plugin.route('/')
 def index():
     items = []
@@ -234,6 +279,13 @@ def index():
     {
         'label': "Rytec xmltv",
         'path': plugin.url_for('rytec_xmltv'),
+        'thumbnail':get_icon_path('settings'),
+    })
+
+    items.append(
+    {
+        'label': "koditvepg.com xmltv",
+        'path': plugin.url_for('koditvepg_xmltv'),
         'thumbnail':get_icon_path('settings'),
     })
 
