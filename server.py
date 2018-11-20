802e53de99d62c484da9e0c7f05d2f86b3a29458
primaeval <primaeval@users.noreply.github.com>
2018-11-20 16:29:59 +0100

    log error

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ def Service():
     if servicing:
         return
     servicing = True
-    xbmc.log("SERVICE")
+    xbmc.log("SERVICE", xbmc.LOGERROR)
     xbmc.executebuiltin('XBMC.RunPlugin(plugin://plugin.program.xmltv.meld/update)')
     time.sleep(2)
     servicing = False
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     try:
         if ADDON.getSetting('service') == 'true':
             monitor = xbmc.Monitor()
-            xbmc.log("[plugin.program.xmltv.meld] service started...", xbmc.LOGDEBUG)
+            xbmc.log("[plugin.program.xmltv.meld] service started...", xbmc.LOGERROR)
             if ADDON.getSetting('service.startup') == 'true':
                 Service()
                 ADDON.setSetting('last.update', str(time.time()))
@@ -42,7 +42,7 @@ if __name__ == '__main__':
                     nextTime = lastTime + datetime.timedelta(seconds=waitTime)
                     td = nextTime - now
                     timeLeft = td.seconds + (td.days * 24 * 3600)
-                    xbmc.log("[plugin.program.xmltv.meld] Service waiting for interval %s" % waitTime, xbmc.LOGDEBUG)
+                    xbmc.log("[plugin.program.xmltv.meld] Service waiting for interval %s" % waitTime, xbmc.LOGERROR)
                 elif ADDON.getSetting('service.type') == '2':
                     next_time = ADDON.getSetting('service.time')
                     if next_time:
@@ -57,10 +57,10 @@ if __name__ == '__main__':
                         timeLeft = td.seconds + (td.days * 24 * 3600)
                 if timeLeft <= 0:
                     timeLeft = 1
-                xbmc.log("[plugin.program.xmltv.meld] Service waiting for %d seconds" % timeLeft, xbmc.LOGDEBUG)
+                xbmc.log("[plugin.program.xmltv.meld] Service waiting for %d seconds" % timeLeft, xbmc.LOGERROR)
                 if timeLeft and monitor.waitForAbort(timeLeft):
                     break
-                xbmc.log("[plugin.program.xmltv.meld] Service now triggered...", xbmc.LOGDEBUG)
+                xbmc.log("[plugin.program.xmltv.meld] Service now triggered...", xbmc.LOGERROR)
                 Service()
                 now = time.time()
                 ADDON.setSetting('last.update', str(now))
